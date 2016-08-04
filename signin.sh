c68a6e8abf47d2c860741e61d1128ea9a42cb73a
Tarun Batra <tarun.batra00@gmail.com>
2016-08-05 01:02:50 +0530

    Makes sign in silent

--- signin.sh
+++ signin.sh
@@ -5,8 +5,7 @@
 
 # Make web call for sign in and dump the response headers in 'headers.txt'
 curl \
--v \
 -D headers.txt \
 -H "Content-Type: application/json" \
 -d "{\"email\": \"$EMAIL\", \"password\": \"$PASSWORD\" }" \
-$SIGN_IN_URL
+$SIGN_IN_URL &> /dev/null
